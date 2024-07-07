c3f5041ee0ed81a8d8db01727ed32fb606443f55
yoojs <yoojs3695@gmail.com>
2024-07-08 05:47:35 +0900

    [완료] 액티비티 연결

--- app/src/main/java/User.kt
+++ app/src/main/java/User.kt
@@ -1,15 +1,16 @@
 import android.os.Parcel
 import android.os.Parcelable
+import com.example.trolls.R
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 open class User(
     open val id: String = "empty user",
     open var password: String = "empty password",
-    open var name: String = "empty name",
+    open var name: String = "이름없음",
     open var nickname: String = "empty nickname",
     open var intro :String = "",
     open val myPosts: MutableList<Post> = mutableListOf(),
     open val myLikes: MutableList<Like> = mutableListOf(),
-    open var profileImageResource: Int = 1
+    open var profileImageResource: Int = R.drawable.no_image
 ) : Parcelable
